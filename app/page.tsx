2257ab515a375da6a1e40ce303c7201358b11e41
Ankitkumar1710 <ankitkumarsingh074@gmail.com>
2023-06-09 22:16:51 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,70 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { BiBath, BiBed, BiHeart } from "react-icons/bi";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 md:p-8 sm:p-24">
+      {/* <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl"> */}
+      <div className="flex flex-col md:flex-row shadow bg-gray-100 rounded-lg ">
+        <img
+          className="object-cover relative md:h-auto md:w-48 rounded-t-lg sm:rounded-l-lg"
+          src="../helicopter-g32998c124_1280.jpg"
+          alt=""
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <BiHeart className="absolute w-7 h-7 mt-2 ml-2 white" color="white" />
+        <div className="flex flex-col gap-x-6  px-4 justify-center pt-2 sm:pt-0 border-r border-gray-300">
+          <div className="flex  gap-x-4 py-1">
+            <p className="sm:text-sm text-xs  text-gray-900 dark:text-white uppercase">
+              DETACHED House
+            </p>
+            <p className=" sm:text-sm text-xs text-gray-900 dark:text-white uppercase">
+              .5Y old
+            </p>
+          </div>
+          <h5 className=" text-3xl font-normal  text-gray-900 dark:text-white">
+            $75,000
+          </h5>
+          <p className="sm:text-sm text-xs text-gray-500 dark:text-gray-400 py-1">
+            742 evergreen terrace
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </div>
+        {/* <div className="flex-none md:flex-col"> */}
+        <div className="flex flex-col items-left justify-center border-r border-gray-300 px-4 py-2 bg-blue-100 md:bg-gray-100 sm:bg-blue-100">
+          <div className="pb-1 sm:pb-2">
+            <p className=" sm:text-sm text-xs text-gray-700 dark:text-white uppercase ">
+              Realtor
+            </p>
+          </div>
+          <div className="flex items-center gap-x-6">
+            <img
+              className="h-12 w-12 rounded-full"
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+            <div>
+              <h3 className="text-sm sm:text-base font-medium leading-6 tracking-tight text-gray-900">
+                Leslie Alexander
+              </h3>
+              <p className="font-normal text-sm sm:text-base text-gray-500">5555-555-000</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-x-6  px-4 justify-center pb-2 sm:pb-0">
+          <div className="flex  gap-x-6">
+            <BiBath className="w-8 h-8 p-1 " />
+            <h3 className="text-base font-medium leading-7 tracking-tight text-gray-900">
+              2 Bathrooms
+            </h3>
+          </div>
+          <div className="flex items-center gap-x-6">
+            <BiBed className="w-8 h-8 p-1" />
+            <h3 className="text-base font-medium leading-7 tracking-tight text-gray-900">
+              2 Bathrooms
+            </h3>
+          </div>
+        </div>
+        {/* </div> */}
       </div>
     </main>
-  )
+  );
 }
